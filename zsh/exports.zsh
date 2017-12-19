308a7fdd68446c5842adce33eebfa38a68a9941d
Devin Schulz <devin@devinschulz.com>
2017-12-19 10:18:48 -0500

    Add go to path

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -3,4 +3,9 @@ export EDITOR=$VISUAL
 
 export GITHUB_USER=devinschulz
 
+export GOPATH=$HOME/go
+export GOROOT=/usr/local/opt/go/libexec
+export PATH=$PATH:$GOPATH/bin
+export PATH=$PATH:$GOROOT/bin
+
 [[ -f ~/exports.local ]] && source ~/exports.local
