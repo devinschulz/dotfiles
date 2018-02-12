cea4c23c1de4e0a5d51179c167cf9c6660d8123d
Devin Schulz <devin@devinschulz.com>
2018-02-12 13:02:21 -0500

    Set tab title to working directory

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -8,4 +8,12 @@ export GOROOT=/usr/local/opt/go/libexec
 export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:$GOROOT/bin
 
+# Set the title of the current tab to the working directory.
+export PROMPT_COMMAND='echo -ne "\033]0;${PWD##*/}\007"'
+
+# Set the iTerm tab title to the current directory when tmux is active.
+if [ $ITERM_SESSION_ID ]; then
+  export PROMPT_COMMAND='echo -ne "\033];${PWD##*/}\007"; ':"$PROMPT_COMMAND";
+fi
+
 [[ -f ~/exports.local ]] && source ~/exports.local
