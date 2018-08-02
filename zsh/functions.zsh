e369701338abb0e8f228fe7973e4c9225bbed098
Devin Schulz <devin@devinschulz.com>
2018-08-02 19:45:25 -0400

    Convert docker aliases to functions

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -8,3 +8,37 @@ mkcd() {
 precmd() {
   echo -ne "\e]1;${PWD##*/}\a"
 }
+
+# Docker
+
+# Start the container in detached mode
+dcu() {
+  [[ -z "$1" ]] && docker-compose up -d $1 || docker-compose up -d
+}
+
+# Build the container
+dcb() {
+  [[ -z "$1" ]] && docker-compose build $1 || docker-compose build
+}
+
+# Stop the container
+dcs() {
+  [[ -z "$1" ]] && docker-compose stop $1 || docker-compose stop
+}
+
+# Restart the container
+# Performs a restart by calling stop and then start. I have ran into issues
+# whenever I only call docker-compse restart.
+dcr() {
+  [[ -z "$1" ]] && dcs $1 && dcu $1 || dcs && dcu
+}
+
+# Stop and remove the container
+dcrm() {
+  [[ -z "$1" ]] && dcs $1 && docker-compose rm -f $1 || dcs && docker-compose rm -f
+}
+
+# Tail the logs of a container
+dcl() {
+  [[ -z "$1" ]] && docker-compose logs -f $1 || docker-compose logs -f
+}
