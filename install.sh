92c869634a357f9c4352e31b49df4e451167a6dc
Devin Schulz <devin@devinschulz.com>
2019-08-29 13:25:50 -0400

    Install some default packages

--- install.sh
+++ install.sh
@@ -7,3 +7,15 @@ stow git
 stow kitty
 stow tmux
 stow vim
+
+# Packages required for language server protocol
+npm i -g \
+    vscode-html-languageserver-bin \
+    dockerfile-language-server-nodejs \
+    typescript \
+    typescript-language-server \
+    javascript-typescript-langserver \
+    vscode-css-languageserver-bin \
+    @elm-tooling/elm-language-server
+
+go get -u golang.org/x/tools/cmd/gopls
