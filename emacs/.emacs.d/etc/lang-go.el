784f20382eebaaafe835ccc3d4ef2f3cbd230fc4
Devin Schulz <devin@devinschulz.com>
2019-09-13 10:32:16 -0400

    Add more go config

--- emacs/.emacs.d/etc/lang-go.el
+++ emacs/.emacs.d/etc/lang-go.el
@@ -1,2 +1,13 @@
 (use-package go-mode
-  :mode "\\.go\\'")
+  :mode "\\.go\\'"
+  :custom (gofmt-command "goimports")
+  :bind (:map go-mode-map
+         ("C-c C-n" . go-run)
+         ("C-c ."   . go-test-current-test)
+         ("C-c f"   . go-test-current-file)
+         ("C-c a"   . go-test-current-project))
+  :config
+  (add-hook 'before-save-hook #'gofmt-before-save)
+  (use-package gotest)
+  (use-package go-tag
+    :config (setq go-tag-args (list "-transform" "camelcase"))))
