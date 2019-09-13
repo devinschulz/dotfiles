a058c5d87cd335380bc24fdbb163c3a701e0f389
Devin Schulz <devin@devinschulz.com>
2019-09-13 10:33:13 -0400

    Add editor editing packages

--- emacs/.emacs.d/etc/editor-edit.el
+++ emacs/.emacs.d/etc/editor-edit.el
@@ -1,3 +1,17 @@
+(prefer-coding-system 'utf-8)
+(set-charset-priority 'unicode)
+
+(defun display-startup-echo-area-message ()
+  (message ""))
+
+(setq default-process-coding-system '(utf-8-unix . utf-8-unix))
+(setq delete-by-moving-to-trash t)
+(setq track-eol t) ; Keep cursor at end of lines
+(setq line-move-visual nil) ; To be required by track-eol
+
+(setq-default kill-whole-line t)	; Kill line including '\n'
+(setq-default indent-tabs-mode nil)   ; use space
+
 (use-package multiple-cursors
   :ensure t
   :bind (("C->"           . mc/mark-next-like-this)
@@ -18,6 +32,26 @@
 (use-package comment-dwim-2
   :bind ("M-;" . comment-dwim-2))
 
-(prefer-coding-system 'utf-8)
-(set-charset-priority 'unicode)
-(setq default-process-coding-system '(utf-8-unix . utf-8-unix))
+;; Highlights changes to the buffer caused by commands
+;; such as ‘undo’, ‘yank’/’yank-pop’, etc.
+(use-package volatile-highlights
+  :demand t
+  :diminish volatile-highlights-mode
+  :config
+  (volatile-highlights-mode t))
+
+;; Automatically reload files that was modified by an external program
+(use-package autorevert
+  :diminish
+  :hook (after-init . global-auto-revert-mode))
+
+;; Delete selection when inserting someting
+(use-package delsel
+  :ensure nil
+  :hook (after-init . delete-selection-mode))
+
+(use-package hungry-delete
+  :diminish
+  :hook (after-init . global-hungry-delete-mode)
+  :bind (("<backspace>" . smart-hungry-delete-backward-char)
+         ("<del>" . smart-hungry-delete-forward-char)))
