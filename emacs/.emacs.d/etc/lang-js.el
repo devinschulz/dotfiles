4eae15a7d2c4600b7cf63f4d1ffa5c9309b7db2a
Devin Schulz <devin@devinschulz.com>
2019-09-17 09:54:38 -0400

    Use rjsx-mode instead of js2-mode

--- emacs/.emacs.d/etc/lang-js.el
+++ emacs/.emacs.d/etc/lang-js.el
@@ -1,25 +1,22 @@
-(use-package js2-mode
-  ; :defines flycheck-javascript-eslint-executable
-  :mode (("\\.js\\'" . js2-mode)
-         ("\\.jsx\\'" . js2-jsx-mode)
-         ("\\.prettierrc'" . js2-mode))
-  :interpreter (("node" . js2-mode)
-                ("node" . js2-jsx-mode))
-  :hook ((js2-mode . js2-imenu-extras-mode)
-         (js2-mode . js2-highlight-unused-variables-mode)
-         (js2-mode . add-node-modules-path))
-  :config
-  (setq js-indent-level 2
-        js-basic-offset 2
-        tab-width 2
-        js2-strict-warning-semi-warning nil))
+(use-package rjsx-mode
+   :mode (("\\.js\\'" . rjsx-mode)
+         ("\\.jsx\\'" . rjsx-mode)
+         ("\\.prettierrc'" . rjsx-mode))
+   :hook ((js2-mode . js2-imenu-extras-mode)
+          (js2-mode . js2-highlight-unused-variables-mode)
+          (js2-mode . add-node-modules-path))
+   :config
+   (setq js-indent-level 2
+         js-basic-offset 2
+         tab-width 2
+         js2-strict-warning-semi-warning nil))
 
 (use-package js2-refactor
-  :hook (js2-mode . js2-refactor-mode))
+  :hook (rjxs-mode . js2-refactor-mode))
 
 (use-package add-node-modules-path
   :commands add-node-modules-path
-  :hook (js2-mode typescript-mode))
+  :hook (rjsx-mode typescript-mode))
 
 ;; Run Mocha or Jasmine tests
 (use-package mocha
@@ -32,9 +29,9 @@
   (prettier-js-mode))
 
 (use-package prettier-js
-  :hook ((js2-mode . web-mode-init-prettier-hook)
-         (less-css-mode . web-mode-init-prettier-hook)
-         (css-mode . web-mode-init-prettier-hook))
+  ;; :hook ((rjsx-mode . web-mode-init-prettier-hook)
+  ;;        (less-css-mode . web-mode-init-prettier-hook)
+  ;;        (css-mode . web-mode-init-prettier-hook))
   :bind ("C-c p" . prettier-js)
   :config
   (setq prettier-js-args '("--trailing-comma" "none" "--single-quote" "true" "--no-semi" "true" "--config-precedence" "prefer-file")))
