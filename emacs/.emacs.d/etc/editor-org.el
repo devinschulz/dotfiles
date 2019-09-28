8f2abba4f058c616803f448b286d11c2eb7de6ec
Devin Schulz <devin@devinschulz.com>
2019-09-27 22:25:21 -0400

    Add org bullets package and update org journal dir

--- emacs/.emacs.d/etc/editor-org.el
+++ emacs/.emacs.d/etc/editor-org.el
@@ -3,6 +3,9 @@
 (use-package org-journal
   :after org
   :config
-  (setq org-journal-dir "~/Google\ Drive/org/journal/"
+  (setq org-journal-dir "~/notes/journal/"
         org-journal-file-format "%Y-%m-%d.org"
         org-journal-date-format "%Y-%m-%d, %A"))
+
+(use-package org-bullets
+  :hook (org-mode . (lambda () (org-bullets-mode 1))))
