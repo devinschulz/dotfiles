135646b5480da8ba2df500d3754bfb694a8a087a
Devin Schulz <devin@devinschulz.com>
2019-10-22 22:16:56 -0400

    Remove org mode bullets

--- emacs/.emacs.d/etc/editor-org.el
+++ emacs/.emacs.d/etc/editor-org.el
@@ -6,6 +6,3 @@
   (setq org-journal-dir "~/notes/journal/"
         org-journal-file-format "%Y-%m-%d.org"
         org-journal-date-format "%Y-%m-%d, %A"))
-
-(use-package org-bullets
-  :hook (org-mode . (lambda () (org-bullets-mode 1))))
