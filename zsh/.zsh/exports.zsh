3a64bc83068eec2dcc4d1a42eba38f763a31c4f2
Devin Schulz <devin@devinschulz.com>
2020-01-02 10:53:26 -0500

    Add N_PREFIX env var

--- zsh/.zsh/exports.zsh
+++ zsh/.zsh/exports.zsh
@@ -8,6 +8,9 @@ export GOROOT=/usr/local/opt/go/libexec
 export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:$GOROOT/bin
 
+# Added by n-install (see http://git.io/n-install-repo).
+export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"
+
 # Set the title of the current tab to the working directory.
 export PROMPT_COMMAND='echo -ne "\033]0;${PWD##*/}\007"'
 
