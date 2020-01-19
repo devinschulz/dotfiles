e8ae20b0eb2b5966fb4013b64031902f1e634140
Devin Schulz <devin@devinschulz.com>
2020-01-18 22:03:43 -0500

    Fix broken GOPATH in linux. Switch to go modules

--- install.sh
+++ install.sh
@@ -17,4 +17,4 @@ npm i -g \
     vscode-css-languageserver-bin \
     eslint_d
 
-go get -u golang.org/x/tools/cmd/gopls
+GO111MODULE=on go get golang.org/x/tools/gopls@latest
--- zsh/.zsh/exports.zsh
+++ zsh/.zsh/exports.zsh
@@ -2,11 +2,14 @@ export VISUAL=emacs
 export EDITOR=$VISUAL
 
 export GITHUB_USER=devinschulz
-
 export GOPATH=$HOME/go
-export GOROOT=/usr/local/opt/go/libexec
 export PATH=$PATH:$GOPATH/bin
-export PATH=$PATH:$GOROOT/bin
+
+if [[ "$OSTYPE" == "darwin" ]]; then
+    # Homebrew path
+    export GOROOT=/usr/local/opt/go/libexec
+    export PATH=$PATH:$GOROOT/bin
+fi
 
 # Set the title of the current tab to the working directory.
 export PROMPT_COMMAND='echo -ne "\033]0;${PWD##*/}\007"'
