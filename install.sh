5d980a97ef608c038e278a252e649dd9c1cfaa2b
Devin Schulz <devin@devinschulz.com>
2020-01-27 09:54:04 -0500

    Install additional language servers

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ stow vim
 
 # Packages required for language server protocol
 npm i -g \
+    bash-language-server \
     dockerfile-language-server-nodejs \
     eslint_d \
     javascript-typescript-langserver \
@@ -16,7 +17,11 @@ npm i -g \
     typescript-language-server \
     vim-language-server \
     vscode-css-languageserver-bin \
-    vscode-html-languageserver-bin
+    vscode-html-languageserver-bin \
+    vscode-json-languageserver \
+    vue-language-server \
+    yaml-language-server
+
 
 # Golang language server
 GO111MODULE=on go get golang.org/x/tools/gopls@latest
