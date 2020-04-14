a34f8507cb17ddb39b541478010e3bd75c5113a6
Devin Schulz <devin@devinschulz.com>
2020-04-14 11:12:25 -0400

    Set NVM_DIR env variable

--- zsh/.zsh/exports.zsh
+++ zsh/.zsh/exports.zsh
@@ -22,4 +22,8 @@ if [ $ITERM_SESSION_ID ]; then
   export PROMPT_COMMAND='echo -ne "\033];${PWD##*/}\007"; ':"$PROMPT_COMMAND"
 fi
 
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
 xsource ~/exports.local
