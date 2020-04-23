8c1e7bbd91506c16b1bfc8c14ec3fe8b7068a6a9
Devin Schulz <devin@devinschulz.com>
2020-04-22 21:45:20 -0400

    Remove custom deft functions

--- emacs/.emacs.d/etc/editor-org.el
+++ emacs/.emacs.d/etc/editor-org.el
@@ -12,41 +12,14 @@
         org-journal-date-format "%Y-%m-%d, %A"))
 
 (use-package deft
-  :after org
-  :bind
-  ("C-c n d" . deft)
-  ("C-c n l" . dev/get-linked-files)
-  ("C-c n i" . dev/insert-zettel)
+  :commands deft
+  :bind ("<f8>" . deft)
   :config
   (setq zettel-indicator "§")
   (setq deft-extensions '("org" "md" "txt"))
   (setq deft-default-extension "txt")
   (setq deft-directory (concat org-directory "/braindump"))
-  (setq deft-recursive t)
-
-  (defun dev/insert-zettel (file-name)
-    "Finds a file, inserts it as a link with the base file name as the link name, and adds the zd-link-indicator I use to the front."
-    (interactive (list (completing-read "File: " (deft-find-all-files-no-prefix))))
-    (org-insert-link nil (concat "file:" (file-name-base file-name) "." (file-name-extension file-name)) (concat zettel-indicator (file-name-base file-name))))
-
-  (defun dev/get-linked-files ()
-    "Show links to this file."
-    (interactive)
-    (let* ((search-term (file-name-nondirectory buffer-file-name))
-           (files deft-all-files)
-	         (tnames (mapcar #'file-truename files)))
-      (multi-occur
-       (mapcar (lambda (x)
-	               (with-current-buffer
-		                 (or (get-file-buffer x) (find-file-noselect x))
-		               (widen)
-		               (current-buffer)))
-	             files)
-       search-term
-       3))))
+  (setq deft-recursive t))
 
 (use-package zetteldeft
-  :after deft
-  :config
-  (setq zetteldeft-id-format "%Y%m%d%H%M")
-  (setq zetteldeft-id-regex "[0-9]\\{12\\}"))
+  :after deft)
