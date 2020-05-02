9352976be40e6379f0db902930fd1c3827137204
Devin Schulz <devin@devinschulz.com>
2020-05-01 21:23:37 -0400

    Remove unused packages

--- emacs/.emacs.d/etc/lang-fish.el
+++
@@ -1,2 +0,0 @@
-(use-package fish-mode
-  :mode "\\.fish'")
--- emacs/.emacs.d/etc/lang-rust.el
+++ emacs/.emacs.d/etc/lang-rust.el
@@ -1,6 +1,1 @@
 (use-package rust-mode)
-
-;; Provide a language server for Rust, and a company backend
-(use-package racer
-  :init
-  (add-hook 'rust-mode #'racer-mode))
