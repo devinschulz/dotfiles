5ee179b4db3f7f54ecfd9cf911692f55b4e308d6
Devin Schulz <devin@devinschulz.com>
2023-02-21 21:14:50 -0500

    Remove treehopper

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,5 @@
 return {
   {
-    "mfussenegger/nvim-treehopper",
-    keys = { { "m", mode = { "o", "x" } } },
-    config = function()
-      vim.cmd([[
-        omap     <silent> m :<C-U>lua require('tsht').nodes()<CR>
-        xnoremap <silent> m :lua require('tsht').nodes()<CR>
-      ]])
-    end,
-  },
-  {
     "nvim-treesitter/nvim-treesitter-context",
     event = "BufReadPre",
     config = true,
