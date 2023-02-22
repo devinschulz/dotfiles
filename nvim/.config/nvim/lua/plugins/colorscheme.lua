5039ba231e0f820b85de5e0a097c2493f9685d5c
Devin Schulz <devin@devinschulz.com>
2023-02-21 21:14:44 -0500

    Update colourscheme, use mini everywhere

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -6,10 +6,42 @@ return {
     priority = 1000,
     opts = {
       flavour = "macchiato",
+      integrations = {
+        cmp = true,
+        dashboard = true,
+        gitsigns = true,
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        mini = true,
+        nvimtree = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
     },
     config = function(_, opts)
       require("catppuccin").setup(opts)
       vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        theme = "catppuccin",
+      },
+    },
+  },
+  {
+    "akinsho/bufferline.nvim",
+    dependencies = {
+      "catppuccin/nvim",
+    },
+    config = function(_, opts)
+      opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
+      require("bufferline").setup(opts)
     end,
   },
 }
--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -22,19 +22,39 @@ return {
     event = "BufReadPost",
     config = function()
       local scrollbar = require("scrollbar")
-      local colors = require("tokyonight.colors").setup()
+      local colors = require("catppuccin.palettes").get_palette("macchiato")
       scrollbar.setup({
-        handle = { color = colors.bg_highlight },
+        handle = { color = colors.surface1 },
         excluded_filetypes = { "prompt", "TelescopePrompt", "noice", "notify" },
         marks = {
-          Search = { color = colors.orange },
-          Error = { color = colors.error },
-          Warn = { color = colors.warning },
-          Info = { color = colors.info },
-          Hint = { color = colors.hint },
-          Misc = { color = colors.purple },
+          Search = { color = colors.green },
+          Error = { color = colors.red },
+          Warn = { color = colors.yellow },
+          Info = { color = colors.blue },
+          Hint = { color = colors.flamingo },
+          Misc = { color = colors.mauve },
         },
       })
     end,
+  },
+  {
+    "folke/noice.nvim",
+    opts = {
+      messages = {
+        view = "mini",
+        view_error = "mini",
+        view_warn = "mini",
+        view_history = "messages",
+      },
+      notify = {
+        view = "mini",
+      },
+      lsp = {
+        -- Messages shown by lsp servers
+        message = {
+          view = "mini",
+        },
+      },
+    },
   },
 }
