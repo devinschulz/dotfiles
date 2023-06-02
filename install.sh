d825586f0e447b87eecbce2c68e86f0258ecb265
Devin Schulz <devin@devinschulz.com>
2023-06-02 11:53:00 -0400

    Add language servers

--- install.sh
+++ install.sh
@@ -5,13 +5,15 @@ stow fish
 stow git
 stow tmux
 stow nvim
+stow helix
 
 npm i -g vscode-langservers-extracted \
-  typescript \ 
-	svelte-language-server
+  typescript \
+	svelte-language-server \
+	dockerfile-language-server-nodejs \
+	bash-language-server
 
 # Check if tmux package manager is installed, if not, install it
 [ ! -d "$HOME/.tmux/plugins/tmp" ] &&
 	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-cargo install taplo-cli --locked --features lsp
