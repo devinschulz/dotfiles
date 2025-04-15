6b173dd6a8c57da2f9607874ef61927d4acbcce3
Devin Schulz <devin@devinschulz.com>
2025-04-15 08:25:28 -0400

    Add zellij

--- install.sh
+++ install.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-stow alacritty
+stow ghostty
 stow fish
 stow git
 stow tmux
 stow nvim
 stow helix
+stow zellij
 
 npm i -g vscode-langservers-extracted \
   typescript \
 	svelte-language-server \
 	dockerfile-language-server-nodejs \
-	bash-language-server
+	bash-language-server \
+	@github/copilot-language-server \
+	prettier
 
 # Check if tmux package manager is installed, if not, install it
-[ ! -d "$HOME/.tmux/plugins/tmp" ] &&
-	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+[ -d "$HOME/.tmux/plugins/tpm" ] || git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
